5337fe592c0a975deb1a3ff845000323cd02bd00
dalwer001 <dalwer.official@gmail.com>
2021-08-07 13:05:49 +0600

    add all

---
+++ assets/js/button.js
@@ -0,0 +1,6 @@
+const videoButton = document.getElementById("videoButton");
+
+videoButton.addEventListener('click',()=>{
+    const videoButtonArea = document.getElementById("videoArea");
+    videoButtonArea.style.display = "none";
+})
